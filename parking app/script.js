00a224b0aeb5b130e98e55a292febaa69c1574b9
ikalin00 <ikalin00@fesb.hr>
2020-07-11 23:51:43 +0200

    Add files via upload

---
+++ parking app/script.js
@@ -0,0 +1,71 @@
+function boja1(value) {
+    let x = [5]
+
+    if (value == 1) {
+        x[0] = document.getElementById("senzor1")
+        x[0].style.fill = "red"
+    }
+    else if (value == 2) {
+        x[1] = document.getElementById("senzor2")
+        x[1].style.fill = "red"
+    }
+
+    else if (value == 3) {
+        x[2] = document.getElementById("senzor3")
+        x[2].style.fill = "red"
+    }
+
+    else if (value == 4) {
+        x[3] = document.getElementById("senzor4")
+        x[3].style.fill = "red"
+    }
+
+    else if (value == 5) {
+        x[4] = document.getElementById("senzor5")
+        x[4].style.fill = "red"
+    }
+
+    else {
+        x[5] = document.getElementById("senzor6")
+        x[5].style.fill = "red"
+    }
+
+
+
+}
+
+function boja2(value) {
+    let x = [5]
+
+    if (value == 1) {
+        x[0] = document.getElementById("senzor1")
+        x[0].style.fill = "rgb(39, 241, 39)"
+    }
+    else if (value == 2) {
+        x[1] = document.getElementById("senzor2")
+        x[1].style.fill = "rgb(39, 241, 39)"
+    }
+
+    else if (value == 3) {
+        x[2] = document.getElementById("senzor3")
+        x[2].style.fill = "rgb(39, 241, 39)"
+    }
+
+    else if (value == 4) {
+        x[3] = document.getElementById("senzor4")
+        x[3].style.fill = "rgb(39, 241, 39)"
+    }
+
+    else if (value == 5) {
+        x[4] = document.getElementById("senzor5")
+        x[4].style.fill = "rgb(39, 241, 39)"
+    }
+
+    else {
+        x[5] = document.getElementById("senzor6")
+        x[5].style.fill = "rgb(39, 241, 39)"
+    }
+
+
+
+}
